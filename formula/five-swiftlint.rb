71fe4d2add003dc7b0b723bd68c70e6303490c97
Tino Emer <tino.emer@five.agency>
2022-09-26 11:33:44 +0200

    formula file change

--- formula/five-swiftlint.rb
+++ formula/five-swiftlint.rb
@@ -1,14 +1,21 @@
-class Swiftlint < Formula
+class FiveSwiftlint < Formula
     desc "Forked Tool to enforce FIVE's Swift style and conventions"
     homepage "https://github.com/vexonius/SwiftLint"
     url "https://github.com/vexonius/SwiftLint/releases/download/beta/five-swiftlint.tar.gz"
     license "MIT"
+    sha256 "b70d68181e31b26a7904d4e4cd6d86f2494ab99e820fe746e6e7710d4e718b13"
     head "https://github.com/vexonius/SwiftLint", branch: "feature/custom-rules"
+    version "1.0.0"
   
     depends_on xcode: ["13.3", :build]  
     uses_from_macos "swift"
   
     def install
         bin.install "five-swiftlint"
+    end
+
+      # Homebrew requires tests.
+    test do
+        assert_match "five-swiftlint version 1.0.0", shell_output("#{bin}/five-swiftlint -v", 2)
     end
   end
