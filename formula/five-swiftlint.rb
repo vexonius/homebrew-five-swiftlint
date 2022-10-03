bb67bf8641d70da4ce42e78475634e7728c66b44
Tino Emer <tino.emer@five.agency>
2022-10-03 10:58:47 +0200

    Testing intel branch

--- formula/five-swiftlint.rb
+++ formula/five-swiftlint.rb
@@ -1,9 +1,8 @@
 class FiveSwiftlint < Formula
     desc "Forked Tool to enforce FIVE's Swift style and conventions"
     homepage "https://github.com/vexonius/SwiftLint"
-    url "https://github.com/vexonius/SwiftLint/releases/download/beta/five-swiftlint.tar.gz"
+    url "https://github.com/vexonius/SwiftLint/releases/download/x86_64/five-swiftlint-intel.tar.gz"
     license "MIT"
-    sha256 "b70d68181e31b26a7904d4e4cd6d86f2494ab99e820fe746e6e7710d4e718b13"
     head "https://github.com/vexonius/SwiftLint", branch: "feature/custom-rules"
     version "1.0.0"
   
